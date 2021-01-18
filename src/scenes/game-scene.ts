81daa4a39bd7d80d77f9b5d46c8f60dace8421c1
Zbynek Dusatko <zzdobrusky@gmail.com>
2021-01-18 11:22:58 -0700

    added WSAD keys

--- src/scenes/game-scene.ts
+++ src/scenes/game-scene.ts
@@ -7,10 +7,18 @@ const sceneConfig: Phaser.Types.Scenes.SettingsConfig = {
   key: 'Game',
 };
 
+type WSADKeys = {
+  up: Phaser.Input.Keyboard.Key;
+  down: Phaser.Input.Keyboard.Key;
+  left: Phaser.Input.Keyboard.Key;
+  right: Phaser.Input.Keyboard.Key;
+};
+
 export class GameScene extends Phaser.Scene {
   public speed = 200;
 
   private cursorKeys: Phaser.Types.Input.Keyboard.CursorKeys;
+  private wsadKeys: WSADKeys;
   private image: Phaser.Physics.Arcade.Sprite;
 
   constructor() {
@@ -23,22 +31,30 @@ export class GameScene extends Phaser.Scene {
 
     // This is a nice helper Phaser provides to create listeners for some of the most common keys.
     this.cursorKeys = this.input.keyboard.createCursorKeys();
+
+    // added WSAD keys
+    this.wsadKeys = {
+      up: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.W),
+      down: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.S),
+      left: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.A),
+      right: this.input.keyboard.addKey(Phaser.Input.Keyboard.KeyCodes.D),
+    };
   }
 
   public update(): void {
     // Every frame, we create a new velocity for the sprite based on what keys the player is holding down.
     const velocity = new Phaser.Math.Vector2(0, 0);
 
-    if (this.cursorKeys.left.isDown) {
+    if (this.wsadKeys.left.isDown || this.cursorKeys.left.isDown) {
       velocity.x -= 1;
     }
-    if (this.cursorKeys.right.isDown) {
+    if (this.wsadKeys.right.isDown || this.cursorKeys.right.isDown) {
       velocity.x += 1;
     }
-    if (this.cursorKeys.up.isDown) {
+    if (this.wsadKeys.up.isDown || this.cursorKeys.up.isDown) {
       velocity.y -= 1;
     }
-    if (this.cursorKeys.down.isDown) {
+    if (this.wsadKeys.down.isDown || this.cursorKeys.down.isDown) {
       velocity.y += 1;
     }
 
